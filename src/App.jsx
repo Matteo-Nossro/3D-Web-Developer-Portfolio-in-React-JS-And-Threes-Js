421a573e71ab8a10e19c0ee7858f14797ac92754
Matteo-Nossro <matnoss@gmail.com>
2023-03-12 00:12:48 +0100

    build(All): Avancement jusqua la minute 25

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import React from 'react'
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Experience from "./components/Experience.jsx";
+import Tech from "./components/Tech.jsx";
+import About from "./components/About.jsx";
+import Works from "./components/Works.jsx";
+import Feedbacks from "./components/Feedbacks.jsx";
+import Contact from "./components/Contact.jsx";
+import Stars from "./components/canvas/Stars.jsx";
+
 
 
 const App = () => {
 
   return (
-    <div>
-        3d portfolio
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                <Navbar/>
+                <Hero/>
+            </div>
+            <About/>
+            <Experience/>
+            <Tech/>
+            <Works/>
+            <Feedbacks/>
+            <div className="relative z-0">
+                <Contact/>
+                <Stars/>
+            </div>
+        </div>
+      </BrowserRouter>
 
-    </div>
   )
 }
 
