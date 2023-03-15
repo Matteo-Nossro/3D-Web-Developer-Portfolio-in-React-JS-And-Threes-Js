3bd7aaf866f96117a5ed9f8d3d292e8e2883f1a4
Matteo-Nossro <matnoss@gmail.com>
2023-03-15 16:13:22 +0100

    feat(all): vidéo jusqua minute 58
    ajout du desktop en 3D et tes avec une bugatti
    ajout du model de la
    bugatti

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload,
+    useGLTF  // extension qui permet d'importer des modèles 3D
+} from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+
+    // Création du composant qui va contenir le modèle 3D
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+  // const computer = useGLTF('./car/scene.gltf');
+
   return (
-    <div>Computers</div>
-  )
-}
+      // Toutes les balises viennent de react-three-fiber
+      //https://docs.pmnd.rs/react-three-fiber/getting-started/introduction
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor='black' />
+        <spotLight
+            position={[-20, 50, 10]}
+            angle={0.12}
+            penumbra={1}
+            intensity={20}
+            castShadow
+            shadow-mapSize={1024}
+        />
+        <pointLight intensity={1} />
+        {/*<ambientLight intensity={1} />*/}
+        <primitive   // représente le bureau et l'ordinateur (le modèle 3D)
+            object={computer.scene}
+            scale={isMobile ? 0.7 : 0.75}
+            // scale={isMobile ? 0.7 : 2.2}
+            position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+            // position={isMobile ? [0, -3, -2.2] : [0, -3.25, 0]}
 
-export default Computers
+            rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+      <Canvas
+          frameloop='demand'
+          shadows
+          dpr={[1, 2]}
+          camera={{ position: [20, 3, 5], fov: 25 }}
+          gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls   // permet de faire tourner la caméra
+              enableZoom={false}
+              maxPolarAngle={Math.PI / 2}
+              minPolarAngle={Math.PI / 2}
+              // minAzimuthAngle={-Math.PI / 4}
+              // maxAzimuthAngle={Math.PI / 4}
+              // minPolarAngle={Math.PI / 2}
+              // maxPolarAngle={Math.PI - Math.PI / 2}
+          />
+          <Computers isMobile={isMobile} />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+  );
+};
+
+export default ComputersCanvas;
